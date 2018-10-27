64c58d79b4da14fef9edf5bb1ab214f3ea7df8e7
0Calories <cppanand@gmail.com>
2018-10-26 22:08:12 -0400

    Fix hardcoded report score values, will now go in proper column

--- cwsreactapp/src/components/NewFunctionalScore/impairment.jsx
+++ cwsreactapp/src/components/NewFunctionalScore/impairment.jsx
@@ -111,14 +111,19 @@ class Impairment extends Component {
 
     const object = {
       careProvider: "testProvider",
-      ModerateImpairment: this.state.selectedScore.value,
       domain: this.state.selectedOption.label,
       subDomain: this.state.selectedOption2.label,
       comment: this.state.c,
-      assessmentDate: this.state.date.format("DD-MMM-YY")
+      assessmentDate: this.state.date.format("DD-MMM-YY"),
+      ...(this.state.selectedScore.value == 0) && { NoImpairment: 0 },
+      ...(this.state.selectedScore.value == 1) && { MildImpairment: 1 },
+      ...(this.state.selectedScore.value == 2) && { ModerateImpairment: 2 },
+      ...(this.state.selectedScore.value == 3) && { SevereImpairment: 3 },
+      ...(this.state.selectedScore.value == 4) && { CompleteImpairment: 4 },
+
     };
     console.log(object);
-    alert("submitted");
+    alert("Report submitted successfully");
     postRef.push(object);
   };
 
